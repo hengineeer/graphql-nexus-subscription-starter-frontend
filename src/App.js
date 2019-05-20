722ef32fde4620e41ebfec4e052626e79406cbf0
Henry Yang <henry@novvum.io>
2019-05-20 14:01:06 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  InputGroup,
+  InputGroupAddon,
+  Input,
+  Card,
+  CardText,
+  CardBody,
+  Alert
+} from "reactstrap";
+
+class ChatBox extends React.Component {
+  render() {
+    return (
+      <div style={{ width: "95%" }}>
+        <Card>
+          <CardBody
+            style={{
+              backgroundColor: "#333",
+              padding: "50px"
+            }}
+          >
+            <CardText>
+              <Alert
+                color="secondary"
+                style={{ fontSize: "15px", width: "100%" }}
+              >
+                <b>NAME: </b>
+                Content content
+              </Alert>
+              <Alert
+                color="secondary"
+                style={{ fontSize: "15px", width: "100%" }}
+              >
+                <b>NAME: </b>
+                Content content
+              </Alert>
+              <Alert
+                color="secondary"
+                style={{ fontSize: "15px", width: "100%" }}
+              >
+                <b>NAME: </b>
+                Content content
+              </Alert>
+            </CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>GraphQL Nexus Subscriptions</h1>
+        <InputGroup style={{ margin: "50px", width: "50%" }}>
+          <InputGroupAddon addonType="prepend">@</InputGroupAddon>
+          <Input placeholder="Username" />
+        </InputGroup>
+        <ChatBox />
+        <InputGroup style={{ margin: "50px", width: "75%" }}>
+          <InputGroupAddon addonType="prepend">✎</InputGroupAddon>
+          <Input placeholder="Your Message Here" />
+        </InputGroup>
       </header>
     </div>
   );
