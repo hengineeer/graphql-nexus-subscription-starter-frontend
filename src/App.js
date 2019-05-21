6db80392da5b967a44413de57a4696f74840d82c
Henry Yang <henry@novvum.io>
2019-05-21 13:58:29 -0700

    minor styling

--- src/App.js
+++ src/App.js
@@ -15,10 +15,12 @@ import {
 import { ApolloProvider } from "react-apollo";
 import ApolloClient from "apollo-boost";
 
+//Connect backend to frontend
 const client = new ApolloClient({
   uri: "http://localhost:4000"
 });
 
+//Query for existing posts
 const GET_EXISTING_POSTS = gql`
   query PostQuery {
     posts {
@@ -75,18 +77,23 @@ class AppContents extends React.Component {
       <div className="App">
         <header className="App-header">
           <h1>GraphQL Nexus Subscriptions</h1>
-          <InputGroup style={{ margin: "50px", width: "50%" }}>
-            <InputGroupAddon addonType="prepend">@</InputGroupAddon>
-            <Input placeholder="Username" />
-          </InputGroup>
-          <ChatBox />
-          <InputGroup style={{ margin: "50px", width: "75%" }}>
-            <InputGroupAddon addonType="prepend">✎</InputGroupAddon>
-            <Input placeholder="Your Message Here" />
-            <Button outline color="secondary" style={{ marginLeft: "10px" }}>
-              Submit
-            </Button>
-          </InputGroup>
+          <form style={{ width: "80%" }}>
+            <InputGroup
+              style={{ marginBottom: "50px", marginTop: "50px", width: "50%" }}
+            >
+              <InputGroupAddon addonType="prepend">@</InputGroupAddon>
+              <Input placeholder="Username" />
+            </InputGroup>
+            <ChatBox />
+
+            <InputGroup style={{ marginTop: "50px", width: "95%" }}>
+              <InputGroupAddon addonType="prepend">✎</InputGroupAddon>
+              <Input placeholder="Your Message Here" />
+              <Button outline color="secondary" style={{ marginLeft: "10px" }}>
+                Submit
+              </Button>
+            </InputGroup>
+          </form>
         </header>
       </div>
     );
