2e46220451e8c0f4d4aa33365e8f6c4e65fcdcb3
Henry Yang <henry@novvum.io>
2019-05-21 13:32:39 -0700

    query for existing posts

--- src/App.js
+++ src/App.js
@@ -19,6 +19,16 @@ const client = new ApolloClient({
   uri: "http://localhost:4000"
 });
 
+const GET_EXISTING_POSTS = gql`
+  query PostQuery {
+    posts {
+      id
+      author
+      content
+    }
+  }
+`;
+
 class ChatBox extends React.Component {
   render() {
     return (
@@ -31,27 +41,26 @@ class ChatBox extends React.Component {
             }}
           >
             <CardText>
-              <Alert
-                color="secondary"
-                style={{ fontSize: "15px", width: "100%" }}
-              >
-                <b>NAME: </b>
-                Content content
-              </Alert>
-              <Alert
-                color="secondary"
-                style={{ fontSize: "15px", width: "100%" }}
-              >
-                <b>NAME: </b>
-                Content content
-              </Alert>
-              <Alert
-                color="secondary"
-                style={{ fontSize: "15px", width: "100%" }}
-              >
-                <b>NAME: </b>
-                Content content
-              </Alert>
+              <Query query={GET_EXISTING_POSTS}>
+                {({ loading, error, data }) => {
+                  if (loading) return "Loading...";
+                  if (error) return `Error! ${error.message}`;
+                  console.log(data.posts);
+                  return (
+                    <div>
+                      {data.posts.map(post => (
+                        <Alert
+                          color="secondary"
+                          style={{ fontSize: "15px", width: "100%" }}
+                        >
+                          <b>{post.author}: </b>
+                          {post.content}
+                        </Alert>
+                      ))}
+                    </div>
+                  );
+                }}
+              </Query>
             </CardText>
           </CardBody>
         </Card>
