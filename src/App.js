1d080f60d17b49a86da5cd762231c5addeaad4bd
Henry Yang <henry@novvum.io>
2019-06-04 10:32:17 -0700

    added link to websocket

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import React from "react";
 import "./App.css";
 import { ApolloProvider } from "react-apollo";
-import ApolloClient from "apollo-boost";
+import { ApolloClient } from "apollo-boost";
 import AppContents from "./Components/AppContents";
+import { WebSocketLink } from "apollo-link-ws";
+import { split } from "apollo-link";
+import { getMainDefinition } from "apollo-utilities";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-//Connect backend to frontend
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/`,
+  options: {
+    reconnect: true
+  }
+});
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000/"
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpLink
+);
+
 const client = new ApolloClient({
-  uri: "http://localhost:4000"
+  link,
+  cache: new InMemoryCache()
 });
 
 function App() {
+  console.log(client);
   return (
     <ApolloProvider client={client}>
       <AppContents />
